ae2344faa198b35f15f90e50396580ebe5f8fda6
Festanny <60522731+Festanny@users.noreply.github.com>
2023-02-07 14:24:12 +0400

    upd

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
 $('.reviews-slider').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+    ]
 })
 
-sliderSecondBlock()
-$('.reviews .reviewsBlock .slick-arrow').on('click', function() {
+$(window).resize(function () {
+    if ($('body').width() > 1199) {
+        sliderSecondBlock()
+    }
+});
+if ($('body').width() > 1199) {
     sliderSecondBlock()
+}
+$('.reviews .reviewsBlock .slick-arrow').on('click', function() {
+    if ($('body').width() > 1199) {
+        sliderSecondBlock()
+    }
 })
 function sliderSecondBlock() {
     $('.reviews .reviewsBlock .info-block .item.active').removeClass('active')
